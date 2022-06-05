dbb11676484f450c768026f40f8eb1686acedc05
IDystopiaI <dylan.london94@gmail.com>
2022-06-05 23:58:46 +0000

    Completed script
    Script now inserts data into teams and games

--- insert_data.sh
+++ insert_data.sh
@@ -13,7 +13,7 @@ fi
 echo $($PSQL "TRUNCATE teams, games")
 
 # open file and pipe values
-cat games_test.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
 # echo "$YEAR $ROUND $WINNER $OPPONENT $WINNER_GOALS $OPPONENT_GOALS"
 # filling in teams table
@@ -51,16 +51,23 @@ do
     if [[ -z $OPPONENT_ID ]]
     then
       INSERT_OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
       # feedback if opponent is inserted
       if [[ $INSERT_OPPONENT == "INSERT 0 1" ]]
       then
       echo Inserted opponenent: $OPPONENT
       fi
     fi
-  fi
+
+
+# echo winner: $WINNER_ID opponent: $OPPONENT_ID
+
+# above script inserts correct number of teams
 
 # begin working on games table
-
-
+# year round winner_goals opponent_goals [fkey winner_id] [fkey opponent_id]
+# place inside first if clause so that there is no null insert when $WINNER == 'winner'
+  GAME_INSERT=$($PSQL "INSERT INTO games(year, round, winner_goals, opponent_goals, winner_id, opponent_id) VALUES($YEAR, '$ROUND', $WINNER_GOALS, $OPPONENT_GOALS, $WINNER_ID, $OPPONENT_ID)")
+  echo "$GAME_INSERT"
+  fi
 done
